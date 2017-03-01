04bf6b2006526647cf2abe99d67aa712537c58a7
Santiago Basulto <santiago.basulto@gmail.com>
2017-03-01 17:55:15 -0300

    Add a React Native button to demonstrate basic event handling 🕵

--- main.js
+++ main.js
@@ -4,14 +4,24 @@ import {
   StyleSheet,
   Text,
   View,
+  Button  // https://facebook.github.io/react-native/docs/button.html
 } from 'react-native';
 
 class App extends React.Component {
+  onButtonPress(){
+    console.log("Click!")
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.textLarge}>Welcome to rmotr's Workshop!</Text>
         <Text style={styles.textSmall}>(Open up main.js to start working)</Text>
+          <Button
+            onPress={this.onButtonPress.bind(this)}
+            title="Press Me"
+            color="#f1edd2"
+            accessibilityLabel="See an informative alert"
+          />
       </View>
     );
   }
