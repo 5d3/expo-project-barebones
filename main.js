f142f36eaddaafa670c276749df89c221494172f
Santiago Basulto <santiago.basulto@gmail.com>
2017-03-01 17:41:57 -0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,37 @@
+import Exponent from 'exponent';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textLarge}>Welcome to rmotr's Workshop!</Text>
+        <Text style={styles.textSmall}>(Open up main.js to start working)</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#242434',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textLarge: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#21b274'
+  },
+  textSmall: {
+    color: '#21b274'
+  }
+});
+
+Exponent.registerRootComponent(App);
