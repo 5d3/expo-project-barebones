fde7ea0b0335eb78e72086b50d14510a5b8b7843
Santiago Basulto <santiago.basulto@gmail.com>
2017-03-27 13:40:45 -0300

    Project barebones

--- main.js
+++ main.js
@@ -3,30 +3,15 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View,
-  Button,
-  Image  // https://facebook.github.io/react-native/docs/image.html
+  View
 } from 'react-native';
 
 class App extends React.Component {
-  onButtonPress(){
-    console.log("Click!")
-  }
   render() {
     return (
       <View style={styles.container}>
-        <Image
-          style={styles.logo}
-          source={{uri: 'https://cloud.githubusercontent.com/assets/872296/23481309/c189d68e-fea9-11e6-83be-9e1ec5b53e61.png'}}
-        />
         <Text style={styles.textLarge}>Welcome to rmotr's Workshop!</Text>
         <Text style={styles.textSmall}>(Open up main.js to start working)</Text>
-          <Button
-            onPress={this.onButtonPress.bind(this)}
-            title="Press Me"
-            color="#f1edd2"
-            accessibilityLabel="See an informative alert"
-          />
       </View>
     );
   }
